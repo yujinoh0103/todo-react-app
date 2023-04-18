fead783f53d5394ca3fbd80d6fcd3ee055a834ae
Oh Yu Jin <yujin0103@sookmyung.ac.kr>
2023-04-18 14:08:25 +0900

    6 안녕하세요! 출력하기

---
+++ src/Hello.js
@@ -0,0 +1,9 @@
+import React from 'react';
+class Hello extends React.Component { render(){
+    return (
+        <div className="Hello">
+        <label for="hello"> 안녕하세요!</label>
+        </div>
+        ); }
+       }
+       export default Hello;
